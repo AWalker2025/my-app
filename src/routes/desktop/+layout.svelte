e806bab7bf56fb2ca9af3fad367012a833f467a7
AWalker2025 <112598458+AWalker2025@users.noreply.github.com>
2023-04-11 15:50:58 -0400

    initial commit

---
+++ src/routes/desktop/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let count;
+</script>
+<div class ="options">
+    <form>
+      <p><b>Filter Results:</b></p>
+     <label for="normal">
+       <input type="radio" id="normal" name="fav_language" value="Normal"> Normal
+     </label><br>
+  
+     <label for="greyscale">
+          <input type="radio" id="greyscale" name="fav_language" value="Greyscale"> Greyscale
+     </label><br>
+  
+     <label for="blur">
+          <input type="radio" id="blur" name="fav_language" value="blur"> Blur
+     </label><br>
+  
+     <label for="both">
+      <input type="radio" id="greyscale and blur" name="fav_language" value="greyscale and blur"> Greyscale and Blur
+  </label><br>
+  </form>
+  <div class="Counter">
+    <p><b>Number of Photos Needed:</b></p>
+    <button>Red</button>
+    <span>{ count }</span>
+  </div>
+</div>
+
+<slot />
---
+++ src/routes/desktop/+page.svelte
@@ -0,0 +1,26 @@
+<h1>Desktop</h1>
+<!-- 
+<script>
+    import { count } from "./+layout.svelte"
+
+    //export let count = 0
+
+    const numberOfImages = 20
+
+    const numbers = Array.from({ length: numberOfImages }).map((_, i) => i)
+</script> -->
+
+<div>
+    <img src="https://picsum.photos/1440/768" alt="random photo">
+</div>
+
+<style>
+    img {
+        max-width: 100%;
+        height: auto;
+    }
+</style>
+
+
+
+
